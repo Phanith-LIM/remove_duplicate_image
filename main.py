6a5c8862032e2412a434f926f9acb716da767758
Phanith LIM <limphanith.office@gmail.com>
2023-05-05 14:37:11 +0700

    done

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import os
 from difPy import dif
 
-search = dif("/Users/limphanith/Documents/Python/pythonProject/image")
+search = dif("/image")
 locations = search.result.values()
 with open('result.txt', 'w') as f:
     f.write('Duplicated files: ' + str(len(locations)) + "\n")
 for location in locations:
-    print('Location: ' + location['location'].replace('/Users/limphanith/Documents/Python/pythonProject/image/', ''))
+    print('Location: ' + location['location'].replace('/image/', ''))
     match = location['matches'].values()
 
     with open('result.txt', 'a') as f:
-        f.write('Location: ' + location['location'].replace('/Users/limphanith/Documents/Python/pythonProject/image/', '') + '\n')
+        f.write('Location: ' + location['location'].replace('/image/', '') + '\n')
 
     for m in match:
-        print("Delete: " + m['location'].replace('/Users/limphanith/Documents/Python/pythonProject/', ''))
+        print("Delete: " + m['location'].replace('/', ''))
         os.remove(m['location'])
         with open('result.txt', 'a') as f:
-            f.write('Delete: ' + m['location'].replace('/Users/limphanith/Documents/Python/pythonProject/image/', '') + '\n\n')
+            f.write('Delete: ' + m['location'].replace('/image/', '') + '\n\n')
         print('\n')
 print("Done")
